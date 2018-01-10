aad7659669a9f3e89360db05c7313e52679f304d
Harry Ickeringill <harry.ickeringill@gmail.com>
2018-01-10 18:06:58 +0800

    Support for custom ratelimiting

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,4 +1,6 @@
 # fortnite.js Changelog
 
+# v1.1.0
+ - `<fortnite>.rateLimit` <Object> added - Custom support for ratelimiting available, reccomended for large scale apps. 30 hits per minute per api key, reseting every time the servers clock ticks to a new minute.
 # v1.0.0
  - First stable release
--- src/Client.js
+++ src/Client.js
@@ -16,12 +16,28 @@ class Client {
             'User-Agent': `fortnite.js v${Package.version} (${Package.homepage})`,
             'TRN-Api-Key': this.key
         };
+        
+        this.rateLimit = {
+            limit: 30,
+            remaining: 30
+        };
+
     }
 
     get(username, platform = 'pc') {
         return snekfetch.get(`https://api.fortnitetracker.com/v1/profile/${platform}/${username.replace(/ /g, '%20')}`)
             .set(this.headers)
-            .then(r => r.body.error ? Promise.reject(r.body.error) : new Account(r.body))
+            .then(r => {
+                
+                this.rateLimit = {
+                    limit: Number(r.headers['x-ratelimit-limit-minute']),
+                    remaining: Number(r.headers['x-ratelimit-remaining-minute'])
+                };
+
+                if (r.body.error || r.body.message) return Promise.reject(r.body.error);
+
+                return new Account(r.body);
+            })
             .catch(e => Promise.reject(`HTTP ${e}`));
     }
 
