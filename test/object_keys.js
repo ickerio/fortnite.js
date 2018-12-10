5b6d88f1217c501393f95d3a408a468bfecf056c
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-12-09 21:48:51 -0600

    Added tests for get() and getMatches()

---
+++ test/object_keys.js
@@ -0,0 +1,71 @@
+exports.USER_INFO_KEYS = [
+  'accountId',
+  'curr_duo',
+  'curr_solo',
+  'curr_squad',
+  'duo',
+  'solo',
+  'squad',
+  'displayName',
+  'platformId',
+  'platformName',
+  'platformNameLong',
+  'recentMatches',
+  'stats',
+];
+
+exports.MODE_KEYS = [
+  'kd',
+  'kills',
+  'kpg',
+  'matches',
+  'score',
+  'scorePerMatch',
+  'top1',
+  'top10',
+  'top12',
+  'top25',
+  'top3',
+  'top5',
+  'top6',
+  'trnRating',
+  'winRatio',
+];
+
+exports.STATS_KEYS = [
+  'kd',
+  'kills',
+  'matches',
+  'score',
+  'top1',
+  'top12',
+  'top25',
+  'top3',
+  'top5',
+  'top6',
+  'winPercent',
+];
+
+exports.MATCH_KEYS = [
+  'dateCollected',
+  'id',
+  'kills',
+  'matches',
+  'playlist',
+  'score',
+  'top1',
+  'top10',
+  'top12',
+  'top25',
+  'top3',
+  'top5',
+  'top6',
+  'trnRating',
+];
+
+exports.RECENT_MATCH_KEYS = [
+  'accountId',
+  'minutesPlayed',
+  'platform',
+  'trnRatingChange',
+];
